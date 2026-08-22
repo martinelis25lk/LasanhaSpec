170625e93a1e6a7810ae6aff0320409b14296c62
martinelis25lk <gui.almeida.edu@gmail.com>
2026-08-22 00:39:20 -0300

    fix: change baseUrl from prod api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+// Trocando o localhost pelo seu domínio de produção + a rota /api
+baseURL: "https://lasanhaspecs.duckdns.org/api",
 });
 
 api.interceptors.request.use((config) => {
