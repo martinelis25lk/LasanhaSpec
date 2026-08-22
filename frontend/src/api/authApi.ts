02eac82354c3669db20434a4809bca8b682b5820
martinelis25lk <gui.almeida.edu@gmail.com>
2026-08-22 01:00:06 -0300

    fix: ajusta API_URL no authApi para producao

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,24 +1,25 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+// Trocamos o localhost pela URL oficial da sua API na AWS
+const API_URL = "https://lasanhaspecs.duckdns.org/api";
 
 type LoginRequest = {
-  email: string;
-  password: string;
+email: string;
+password: string;
 };
 
 type RegisterRequest = {
-  username: string;
-  email: string;
-  password: string;
+username: string;
+email: string;
+password: string;
 };
 
 export const login = async (data: LoginRequest) => {
-  const response = await axios.post(`${API_URL}/auth/login`, data);
-  return response.data;
+const response = await axios.post(`${API_URL}/auth/login`, data);
+return response.data;
 };
 
 export const register = async (data: RegisterRequest) => {
-  const response = await axios.post(`${API_URL}/auth/register`, data);
-  return response.data;
+const response = await axios.post(`${API_URL}/auth/register`, data);
+return response.data;
 };
